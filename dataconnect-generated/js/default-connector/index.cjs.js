eb00246e6a998fc5fd5febff223469b8515fcd1f
Shay <moeshakasanova@gmail.com>
2024-11-01 17:30:06 +0400

    firebase hosting config

---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+const { getDataConnect, validateArgs } = require("firebase/data-connect");
+
+const connectorConfig = {
+  connector: "default",
+  service: "chat-app",
+  location: "us-central1",
+};
+exports.connectorConfig = connectorConfig;
